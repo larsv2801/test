d3c8205c9c932aa145ada534120b22a7e26170f4
larsv2801 <148888518+larsv2801@users.noreply.github.com>
2024-10-01 21:11:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 self.addEventListener('push', function(event) {
-    const data = event.data ? event.data.json() : { title: 'Push Melding', body: 'Er is een nieuwe melding!' };
+    const data = event.data ? event.data.json() : { title: 'Lees!', body: 'Het is tijd om een boek te lezen.' };
 
     const options = {
         body: data.body,
-        icon: 'icon.png',
-        badge: 'badge.png'
+        icon: 'https://cdn-icons-png.flaticon.com/512/8832/8832880.png',
+        badge: 'https://cdn-icons-png.flaticon.com/512/8832/8832880.png'
     };
 
     event.waitUntil(
